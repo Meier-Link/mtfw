3da2318f4bc5340a85b44e3a4d3e608cf0e39fe1
Jérémie 'Meier Link' Balagna-Ranin <jeremie.balagna@gmail.com>
2014-02-28 00:28:59 +0100

    update licence page with the source link

--- htdocs/view/about/licence.php
+++ htdocs/view/about/licence.php
@@ -3,6 +3,5 @@
 <div style="float: left">
   L'ensemble du site - code et contenu - est sous licence Creative Commons - BY SA, dont le texte complet peut être
   retrouvé <a href="http://creativecommons.org/licenses/by-sa/3.0/fr/">ici</a>.<br/>
-  Concernant les sources, celles-ci seront mises à disposition dès que la base du site sera suffisamment complète et
-  surtout suffisamment bien débuggée ...
+  Les sources de ce mini framework sont disponibles sur <a href="https://github.com/Meier-Link/mtfw">Github</a>.
 </div>
