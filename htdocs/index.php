84d8db7b309d76492bfca1c580204a66a4ae7d82
Jérémie 'Meier Link' Balagna-Ranin <jeremie.balagna@gmail.com>
2014-03-04 18:23:55 +0100

    store secure path in a variable pour facilitate code organization

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,7 +2,7 @@
 /*
  *  index.php
  *  
- *  Copyright 2010 Meier Link <lucian.von.ruthven@autistici.org>
+ *  Copyright 2014 Jeremie aka Meier Link <jeremie.balagna@autistici.org>
  *  
  *  This program is free software licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 France License.
  *  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
@@ -13,20 +13,24 @@
 
 session_start();
 
-require_once('../secure/Conf.php');
+// Change this if you want to change code organization
+$SECURE_PATH = "../secure/";
+
+require_once($SECURE_PATH . 'Conf.php');
 
 function __autoload($classname)
 {
+  global $SECURE_PATH;
   if (file_exists("controller/$classname.php"))
     include_once("controller/$classname.php");
   else if (file_exists("model/$classname.php"))
     include_once("model/$classname.php");
   else if ($classname == "DbConnect")
-    include_once("../secure/DbConnect.php");
+    include_once($SECURE_PATH . "DbConnect.php");
   else if ($classname == "Log")
-    include_once("../secure/Log.php");
+    include_once($SECURE_PATH . "Log.php");
   else if ($classname == "StdTools")
-    include_once("../secure/StdTools.php");
+    include_once($SECURE_PATH . "StdTools.php");
 }
 
 $controller = Controller::init();
