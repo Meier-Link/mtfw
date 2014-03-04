78e2d64277389d02bd4c2ab05b5a6991ed0d4096
Jérémie 'Meier Link' Balagna-Ranin <jeremie.balagna@gmail.com>
2014-03-04 16:34:26 +0100

    just remove a french speaking failure

--- htdocs/view/main/home.php
+++ htdocs/view/main/home.php
@@ -1,6 +1,6 @@
 <h2>Un petit framework pour de petits projets</h2>
 <p>
-  Ce site n'est qu'un petit site de démo pour mon framework.<br/>
+  Ce site n'est qu'une petite démo pour mon framework.<br/>
   Il s'agit d'une base que j'utilise déjà depuis quelques temps dans mes projets web.
   Ce projet est donc une sorte d'aboutissement.
 </p>
