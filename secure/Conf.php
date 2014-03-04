4d5b9bf41fdef4fbbb00137f865b55ff782429f1
Jérémie 'Meier Link' Balagna-Ranin <jeremie.balagna@gmail.com>
2014-03-04 14:51:58 +0100

    more explicit version of Conf.php

--- secure/Conf.php
+++ secure/Conf.php
@@ -16,22 +16,34 @@ class Conf
 {
   private static $conf = array(
     /* General values */
-    'SITE_NAME'   => 'MyTinyFrameWork',
-    'SITE_DESC'   => 'A tiny framework for tiny websites',
-    'SITE_OWNER'  => 'Meier Link',
-    'OWNER_MAIL'  => 'lucian.von.ruthven@autistici.org',
+    // The name of your website (displayed in html title
+    'SITE_NAME'     => 'MyTinyFrameWork',
+    // The displayed description on browser
+    'SITE_DESC'     => 'A tiny framework for tiny websites',
+    // Creator of this website
+    'SITE_OWNER'    => 'Meier Link',
+    // His mail address
+    'OWNER_MAIL'    => 'lucian.von.ruthven@autistici.org',
+    // Default address when web mail used without any address
+    'DEFAULT_SEND'  => 'default@virtual.multiverse.cc',
+    // A tag associated to mail title to make difference with your other mails (may be null)
+    'MAIL_TAG'      => '[MTFW]',
+    
     /* Database specific */
-    'DB_PILOT'    => 'mysql',
-    'DB_PATH'     => null,
-    'DB_SERV'     => 'localhost',
-    'DB_NAME'     => 'mtfw',
-    'DB_USER'     => 'mtfw_user',
-    'DB_PASS'     => 'password',
+    'DB_PILOT'      => 'mysql',
+    // Specific to sqlite
+    'DB_PATH'       => null,
+    'DB_SERV'       => 'localhost',
+    'DB_NAME'       => 'mtfw',
+    'DB_USER'       => 'mtfw_user',
+    'DB_PASS'       => 'password',
+    
     /* Admin access */
-    'ADMIN'       => array('LOGIN' => 'root', 'PSSWD'  => 'password'), // Set PSWWD to null if you use db storage
+    'ADMIN'         => array('LOGIN' => 'root', 'PSSWD'  => 'password'), // Set PSWWD to null if you use db storage
+    
     /* HTML template specific */
-    'DEFAULT_CSS' => array('includes/css/template.css'),
-    'DEFAULT_JS'  => array()
+    'DEFAULT_CSS'   => array('includes/css/template.css'),
+    'DEFAULT_JS'    => array()
   );
   
   public static function get($varname)
